f52a754072242edccd4dc768335f9728fdf6efd3
luis121526 <diazdiaz121526@gmail.com>
2021-05-21 21:57:05 -0500

    primer commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<!DOCTYPE html5>
+<head>
+<title>
+Mario's Burguers
+</title>
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/estilo.css" rel="stylesheet">
+</head>
+<body>
+    <div id="log">
+        
+     </div>
+</body>
+<footer>
+</footer>
+</html>
