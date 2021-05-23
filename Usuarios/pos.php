5e9736e433b736db243087bdf5b2e3f2f4795432
luis121526 <diazdiaz121526@gmail.com>
2021-05-22 21:55:24 -0500

    a

---
+++ Usuarios/pos.php
@@ -0,0 +1,15 @@
+<html>
+<!DOCTYPE html5>
+<title></title>
+<head>
+
+</head>
+<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="../css/estilo.css" >
+<script src="../js/jquery.min.js"> </script>
+<body>
+<button class="btn btn-primary">s</button>
+</body>
+<footer>
+</footer>
+</html>
--- index.php
+++ index.php
@@ -4,13 +4,20 @@
 <title>
 Mario's Burguers
 </title>
-<link href="css/bootstrap.min.css" rel="stylesheet">
-<link href="css/estilo.css" rel="stylesheet">
+<!--<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">-->
+<link rel="stylesheet" type="text/css" href="css/estilo.css">
+<link href="css/bootstrap.min.css" type="text/css" rel="stylesheet">
+<script src="js/jquery.min.js"> </script>
 </head>
+<script>
+$(document).ready(function(){
+  $("body").hide().fadeIn(900);
+})
+</script>
 <body>
-    <div id="log">
-        
-     </div>
+    <div class="holi">
+<button>sad</button>
+    </div>
 </body>
 <footer>
 </footer>
