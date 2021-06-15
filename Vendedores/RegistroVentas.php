0906d13fcb9fd1b6ae1fc6361272600ac534a675
luis121526 <diazdiaz121526@gmail.com>
2021-06-15 00:42:12 -0500

    asd

--- Vendedores/RegistroVentas.php
+++ Vendedores/RegistroVentas.php
@@ -25,7 +25,7 @@
         </span>    
         Punto De Venta  </h4>
     </div>
-    <div class="col-10 empresa2">
+    <div class="col-10  empresa2">
         <h4> <span>
         <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user" width="28" height="28" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
@@ -54,7 +54,7 @@
                 <path d="M12 12l-2 -1.12" />
                 <line x1="6" y1="8.6" x2="4" y2="7.5" />
                 </svg>
-            </span>Boton Opcion</button>
+            </span>Dashboard</button>
             <hr>
             <button class="btn btn-info"><span>
             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-3d-cube-sphere" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -72,8 +72,25 @@
                 <path d="M12 12l-2 -1.12" />
                 <line x1="6" y1="8.6" x2="4" y2="7.5" />
                 </svg>
-            </span>Boton Opcion</button>
+            </span>Venta</button>
            <hr>
+           <button class="btn btn-info"><span>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-3d-cube-sphere" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <path d="M6 17.6l-2 -1.1v-2.5" />
+                <path d="M4 10v-2.5l2 -1.1" />
+                <path d="M10 4.1l2 -1.1l2 1.1" />
+                <path d="M18 6.4l2 1.1v2.5" />
+                <path d="M20 14v2.5l-2 1.12" />
+                <path d="M14 19.9l-2 1.1l-2 -1.1" />
+                <line x1="12" y1="12" x2="14" y2="10.9" />
+                <line x1="18" y1="8.6" x2="20" y2="7.5" />
+                <line x1="12" y1="12" x2="12" y2="14.5" />
+                <line x1="12" y1="18.5" x2="12" y2="21" />
+                <path d="M12 12l-2 -1.12" />
+                <line x1="6" y1="8.6" x2="4" y2="7.5" />
+                </svg>
+            </span>Venta</button>
            <br>
            <br>
            <br>
@@ -90,11 +107,49 @@
            <br>
            <br>
            <br>
-           <br>
-           <br>
-           <br>
-           <br>
+          
+         
 
+        </div>
+        
+        <div class="col-5 productos">
+        <br>
+        
+        <div>
+            <form action="#" method="POST">
+            <input type="text" name="busqueda" placeholder="Buscar Articulo" size=50>
+            <input type="submit" name="enviar" value="Buscar" class="btn btn-info btn-sm">
+            </form>
+         </div> 
+         <div class="w-100"></div>
+         <hr>
+        </div>
+        <div class="col-5 productos2">
+        <br>
+        <h4 class="text-center">Articulos</h4>
+        <hr>
+        <button class="btn btn-warning btn-sm">Hamb. Senc. Res</button>
+        <button class="btn btn-warning btn-sm">Hamb. C/Tocino</button>
+        <button class="btn btn-warning btn-sm">Hamb. Hawaina</button>
+        <button class="btn btn-warning btn-sm">Hamb. Dob. Car.</button>
+
+        <div class="w-100"></div>
+        <br>
+        <button class="btn btn-info btn-sm">Hamb. Senc. Pollo</button>
+        <button class="btn btn-info btn-sm">Hamb C/Tocino</button>
+        <button class="btn btn-info btn-sm">Hamb Hawaina</button>
+        <button class="btn btn-info btn-sm">Hamb. Dob. Car.</button>
+        <div class="w-100"></div>
+        <br>
+        <button class="btn btn-warning btn-sm">Hot Dog Sec.</button>
+        <button class="btn btn-warning btn-sm">Hot Dog/Tocino</button>
+        <button class="btn btn-warning btn-sm">Hot Dog/Jam/Ques.</button>
+        <button class="btn btn-warning btn-sm">Hot Dog Mexicana</button>
+        <div class="w-100"></div>
+        <br>
+        <button class="btn btn-info btn-sm">Papas Francesa</button>
+        <button class="btn btn-info btn-sm">Papas c/Salchicha</button>
+       
         </div>
    </section>
     
