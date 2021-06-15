356ab23d27c290ffa40d1605693fd8d8e716815c
luis121526 <diazdiaz121526@gmail.com>
2021-06-14 23:43:07 -0500

    dsad

--- Vendedores/RegistroVentas.php
+++ Vendedores/RegistroVentas.php
@@ -3,106 +3,100 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>POS</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preload" href="../css/bootstrap.min.css" as="style">
-    <link rel="stylesheet" href="../css/bootstrap.min.css" type="text/css">
-    <link rel="preload" href="../css/estilo.css" as="style">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/estilo.css">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet"> 
-    <title>NuevaVenta</title>
 </head>
 <body>
-    <header class="datos row container-fluid">
-    <section class="col-2">
-    <p>Punto De Venta <span>
-    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-report-money" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-    <path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" />
-    <rect x="9" y="3" width="6" height="4" rx="2" />
-    <path d="M14 11h-2.5a1.5 1.5 0 0 0 0 3h1a1.5 1.5 0 0 1 0 3h-2.5" />
-    <path d="M12 17v1m0 -8v1" />
-    </svg>
-    </span></p>
-    </section>
-    <section class="row container-fluid col">
-    <span></span>
-    </section>
-    <section class="datos2 row container-fluid col-2">
-    <div class="content1">
-    <p>Nombre Usuario <span>
-    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-    <circle cx="12" cy="7" r="4" />
-    <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
-    </svg>
-    </span></p>
+<header class="he row container-fluid"> 
+    <div class="col-2 empresa">
+        <h4 class="text-center">
+        <span>
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-codepen" width="28" height="28" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M3 15l9 6l9 -6l-9 -6l-9 6" />
+        <path d="M3 9l9 6l9 -6l-9 -6l-9 6" />
+        <line x1="3" y1="9" x2="3" y2="15" />
+        <line x1="21" y1="9" x2="21" y2="15" />
+        <line x1="12" y1="3" x2="12" y2="9" />
+        <line x1="12" y1="15" x2="12" y2="21" />
+        </svg>
+        </span>    
+        Punto De Venta  </h4>
     </div>
-    </section>
-    </header>
+    <div class="col-10 empresa2">
+        <h4> <span>
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user" width="28" height="28" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <circle cx="12" cy="7" r="4" />
+        <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+        </svg>
+        </span> Usuario</h4>
+    </div>
+</header>
 
-    <main class=" row conteiner-fluid">
-    <section class=" menulat col-2">
-    <nav class="nav flex-column">
-    <button class="nav-link active btn btn-info btn-sm" onclick="location.href='https://www.google.com'"> <span>
-    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-file-plus" width="36" height="36" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-    <path d="M14 3v4a1 1 0 0 0 1 1h4" />
-    <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" />
-    <line x1="12" y1="11" x2="12" y2="17" />
-    <line x1="9" y1="14" x2="15" y2="14" />
-    </svg>
-    </span> Venta Nueva</button>
-    <button class="nav-link active btn btn-info btn-sm" onclick="location.href='https://www.google.com'"> <span>
-    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-file-plus" width="36" height="36" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-    <path d="M14 3v4a1 1 0 0 0 1 1h4" />
-    <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" />
-    <line x1="12" y1="11" x2="12" y2="17" />
-    <line x1="9" y1="14" x2="15" y2="14" />
-    </svg>
-    </span> Venta Nueva</button>
-    <button class="nav-link active btn btn-info btn-sm" onclick="location.href='https://www.google.com'"> <span>
-    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-file-plus" width="36" height="36" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-    <path d="M14 3v4a1 1 0 0 0 1 1h4" />
-    <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" />
-    <line x1="12" y1="11" x2="12" y2="17" />
-    <line x1="9" y1="14" x2="15" y2="14" />
-    </svg>
-    </span> Venta Nueva</button>
-    <button class="nav-link active btn btn-info btn-sm" onclick="location.href='https://www.google.com'"> <span>
-    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-file-plus" width="36" height="36" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-    <path d="M14 3v4a1 1 0 0 0 1 1h4" />
-    <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" />
-    <line x1="12" y1="11" x2="12" y2="17" />
-    <line x1="9" y1="14" x2="15" y2="14" />
-    </svg>
-    </span> Venta Nueva</button>
-    </nav>
-    </section>
-    <section class=" row container-fluid col-10">
-    <p class="col-2">Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-     Fugiat porro laboriosam incidunt distinctio, eaque repudiandae veritatis nesciunt illum dicta a neque 
-    beatae recusandae delectus error accusamus doloremque minus, consectetur earum!</p>
-    <p class="col-2">Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-     Fugiat porro laboriosam incidunt distinctio, eaque repudiandae veritatis nesciunt illum dicta a neque 
-    beatae recusandae delectus error accusamus doloremque minus, consectetur earum!</p>
-    <p class="col-2">Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-     Fugiat porro laboriosam incidunt distinctio, eaque repudiandae veritatis nesciunt illum dicta a neque 
-    beatae recusandae delectus error accusamus doloremque minus, consectetur earum!</p>
-    <p class="col-2">Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-     Fugiat porro laboriosam incidunt distinctio, eaque repudiandae veritatis nesciunt illum dicta a neque 
-    beatae recusandae delectus error accusamus doloremque minus, consectetur earum!</p>
-    <div class="carrito col-4">
-        <h4 class="text-center">Venta Previa</h4>
-        <hr>
-        <hr>
-        
-    </div>
-  
-    </section>
-    </main>
+   <section class="row container-fluid">
+        <div class="col-2 menu">
+            <button class="btn btn-info"><span>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-3d-cube-sphere" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <path d="M6 17.6l-2 -1.1v-2.5" />
+                <path d="M4 10v-2.5l2 -1.1" />
+                <path d="M10 4.1l2 -1.1l2 1.1" />
+                <path d="M18 6.4l2 1.1v2.5" />
+                <path d="M20 14v2.5l-2 1.12" />
+                <path d="M14 19.9l-2 1.1l-2 -1.1" />
+                <line x1="12" y1="12" x2="14" y2="10.9" />
+                <line x1="18" y1="8.6" x2="20" y2="7.5" />
+                <line x1="12" y1="12" x2="12" y2="14.5" />
+                <line x1="12" y1="18.5" x2="12" y2="21" />
+                <path d="M12 12l-2 -1.12" />
+                <line x1="6" y1="8.6" x2="4" y2="7.5" />
+                </svg>
+            </span>Boton Opcion</button>
+            <hr>
+            <button class="btn btn-info"><span>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-3d-cube-sphere" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <path d="M6 17.6l-2 -1.1v-2.5" />
+                <path d="M4 10v-2.5l2 -1.1" />
+                <path d="M10 4.1l2 -1.1l2 1.1" />
+                <path d="M18 6.4l2 1.1v2.5" />
+                <path d="M20 14v2.5l-2 1.12" />
+                <path d="M14 19.9l-2 1.1l-2 -1.1" />
+                <line x1="12" y1="12" x2="14" y2="10.9" />
+                <line x1="18" y1="8.6" x2="20" y2="7.5" />
+                <line x1="12" y1="12" x2="12" y2="14.5" />
+                <line x1="12" y1="18.5" x2="12" y2="21" />
+                <path d="M12 12l-2 -1.12" />
+                <line x1="6" y1="8.6" x2="4" y2="7.5" />
+                </svg>
+            </span>Boton Opcion</button>
+           <hr>
+           <br>
+           <br>
+           <br>
+           <br>
+           <br>
+           <br>
+           <br>
+           <br>
+           <br>
+           <br>
+           <br>
+           <br>
+           <br>
+           <br>
+           <br>
+           <br>
+           <br>
+           <br>
+           <br>
+           <br>
+
+        </div>
+   </section>
+    
 </body>
 </html>
